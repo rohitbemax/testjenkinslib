bcf36207dcd23b7204ec6ec3d37f047f3c5e95d2
Rohit <rohit.bemax@gmail.com>
2021-04-29 23:55:00 +0530

    Added timeout

--- vars/oplib.groovy
+++ vars/oplib.groovy
@@ -1,21 +1,19 @@
-def call(String chartVersion) {
-    //sh "echo libarycalled"
-    //sh "echo GCloudproject- ${GCLOUD_PROJECT}"
-    //sh "echo $chartVersion"
+def call(String chartVersion, String dockerImageTag, int timeout) {
     sh """
         export VERSION=\$(./gradlew properties -q |  grep 'version:' | awk '{print \$2}')
         CORRELATION_ID=\$(dbus-uuidgen)
+        numOfIterations=0
 
         gcloud auth activate-service-account --key-file ${GKE_SA_KEYFILE}
         gcloud config set project "${GCLOUD_PROJECT}"
 
         gcloud pubsub subscriptions delete one-platform-dev-solution-worker-jenkins || true
         gcloud pubsub subscriptions create one-platform-dev-solution-worker-jenkins --topic projects/lucidworks-dev/topics/one-platform-dev --message-filter=\"attributes.correlation_id=\\"\$CORRELATION_ID\\" AND attributes.event_type != \\"CONTINUOUS_DEPLOYMENT_REQUEST\\"\"  || exit 1
-        gcloud pubsub topics publish ${TOPIC_ID} --attribute event_type=CONTINUOUS_DEPLOYMENT_REQUEST,correlation_id=\$CORRELATION_ID --message="{\"component_name\": \"one-platform-solution-worker\",\"helm_chart_name\": \"one-platform-solution-worker\",\"helm_chart_version\": \"$chartVersion\", \"docker_image_tag\": \"\$VERSION-${env.GIT_SHORT_HASH}\" }"
+        gcloud pubsub topics publish ${TOPIC_ID} --attribute event_type=CONTINUOUS_DEPLOYMENT_REQUEST,correlation_id=\$CORRELATION_ID --message="{\"component_name\": \"one-platform-solution-worker\",\"helm_chart_name\": \"one-platform-solution-worker\",\"helm_chart_version\": \"$chartVersion\", \"docker_image_tag\": \"\$VERSION-$dockerImageTag\" }"
 
-        a=1
-        m=12
-        while [ \$a -le \$m ]
+        counter=1
+        numOfIterations=`expr $timeout / 10`
+        while [ \$counter -le \$numOfIterations ]
         do
             CI_RESULT=\$(gcloud pubsub subscriptions pull projects/lucidworks-dev/subscriptions/one-platform-dev-solution-worker-jenkins --auto-ack --format \"value(message.attributes.event_type)\")
             if [ \"\$CI_RESULT\" = \"CONTINUOUS_DEPLOYMENT_SUCCESS\" ]
@@ -29,14 +27,14 @@ def call(String chartVersion) {
                 exit 1
             fi
                                     
-            if [ \$a -eq \$m ]
+            if [ \$counter -eq \$numOfIterations ]
             then
                 echo \"Timed out waiting for results\"
                 gcloud pubsub subscriptions delete one-platform-dev-solution-worker-jenkins
                 exit 1
             fi
                                     
-            a=`expr \$a + 1`
+            counter=`expr \$counter + 1`
             sleep 10
         done
                                 
